91f4f67950d500a8efa6f6a04c56033842147c5b
mamadoz79 <47079265+mamadoz79@users.noreply.github.com>
2021-12-04 23:51:47 +0330

    Update spark.py

--- spark.py
+++ spark.py
@@ -1,19 +1,13 @@
-import docx
 from pyspark import SparkConf, SparkContext
-import re
-import sys
+
 if __name__ == '__main__':
-    file_name = sys.argv[1]
-    doc = docx.Document(f"{file_name}")
-    all_words = [re.split('\t|\\u200f| ', i.text) for i in doc.paragraphs]
-    dictionary_of_first_char = {}
-    for sentences in all_words:
-        for single_word in sentences:
-            if len(single_word):
-                continue
-            try:
-                dictionary_of_first_char[single_word[0]] += 1
-            except:
-                dictionary_of_first_char[single_word[0]] = 1
-    for element in dictionary_of_first_char:
-        print(element, ' contains ' ,dictionary_of_first_char[element], 'characters')
+    sc = SparkContext(conf = SparkConf().setMaster("local"))
+    Input = sc.textFile("turned_to_text.txt")
+    words = Input.flatMap(lambda x: [i[0] for i in x.split()])
+    d = words.countByValue()
+    for i in range(1536, 1791):
+        try:
+            if d[chr(i)]:
+                print(chr(i), ' : ', d[chr(i)])
+        except:
+            pass
